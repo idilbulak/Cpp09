76fe4e056e31f00141e15bcbd30e3a2d045e3d35
Bulak <bulak@Bulaks-iMac.local>
2023-03-14 22:21:20 +0100

    done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,24 +5,27 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "Error: No input expression provided." << std::endl;
+        std::cerr << "Error: No inverted Polish mathematical expression provided." << std::endl;
         return 1;
     }
 
-    std::stack<int> numbers; // stack to store numbers
+    std::stack<int> numbers;
 
-    // convert input expression string to a stringstream
     std::istringstream ss(argv[1]);
 
     std::string token;
-    while (ss >> token) { // loop through all input tokens
+    while (ss >> token) {
         if (token == "+" || token == "-" || token == "*" || token == "/") {
-            if (numbers.size() < 2) { // not enough operands
+            if (numbers.size() < 2) {
                 std::cerr << "Error: Insufficient operands for " << token << " operator." << std::endl;
                 return 1;
             }
             int n2 = numbers.top(); numbers.pop();
             int n1 = numbers.top(); numbers.pop();
+			if (n1 > 10 || n2 > 10) {
+                std::cerr << "Error: numbers bigger than 10." << std::endl;
+                return 1;
+            }
             int result;
             if (token == "+")
                 result = n1 + n2;
@@ -31,7 +34,7 @@ int main(int argc, char* argv[]) {
             else if (token == "*")
                 result = n1 * n2;
             else if (token == "/") {
-                if (n2 == 0) { // division by zero
+                if (n2 == 0) {
                     std::cerr << "Error: Division by zero." << std::endl;
                     return 1;
                 }
@@ -39,17 +42,17 @@ int main(int argc, char* argv[]) {
             }
             numbers.push(result);
         }
-        else { // token is a number
-            int num = atoi(token.c_str()); // convert string to int
+        else {
+            int num = atoi(token.c_str());
             numbers.push(num);
         }
     }
 
-    if (numbers.size() != 1) { // final stack size should be 1
+    if (numbers.size() != 1) {
         std::cerr << "Error: Invalid input expression." << std::endl;
         return 1;
     }
     
-    std::cout << numbers.top() << std::endl; // output final result
+    std::cout << numbers.top() << std::endl;
     return 0;
 }
